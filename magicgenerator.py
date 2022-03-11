14924a8d2e82baa51686ebfa2c8d69ff8d9acd11
dincrash <dincrash@yandex.ru>
2022-03-11 16:46:12 +0300

    add tests

--- magicgenerator.py
+++ magicgenerator.py
@@ -8,8 +8,7 @@ import os
 import configparser
 import uuid
 from multiprocessing import Pool
-from unittest.mock import Mock
-import pytest
+
 
 
 # help
@@ -147,61 +146,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-DATA_SCHEM_FORTEST = "{\"date\": \"timestamp:\",\"name\": \"str:rand\",\"type\": " \
-                      "\"['client', 'partner', 'government']\",\"age\": \"int:rand(1, 15)\"}"
-
-
-@pytest.mark.parametrize("a, expected_result",
-                         [({'data_schema': "./schema.json"}, 100), ({'data_schema': DATA_SCHEM_FORTEST}, 100)])
-def test_datatype(a, expected_result):
-    assert type(read_json(a)['age']) is type(expected_result)
-
-
-@pytest.mark.parametrize("a, expected_result",
-                         [({'data_schema': "./schema.json"}, 100), ({'data_schema': DATA_SCHEM_FORTEST}, 100)])
-def test_dataschema(a, expected_result):
-    assert read_json(a)['age'] <= expected_result
-
-
-def test_temporaryfiles(tmp_path):
-    d = tmp_path / "sub"
-    d.mkdir()
-    p = d / "schema.json"
-    m = Mock()
-    m.main(p)
-
-
-def test_clearpath():
-    mydict = {"path_to_save_files": "./output", "file_name": "super_data777"}
-    out = mydict["path_to_save_files"] + "/" + mydict["file_name"]
-    with open(out, 'w', encoding="cp1251") as f:
-        json.dump(mydict, f, ensure_ascii=False)
-    clear_path(mydict)
-    assert os.path.isfile(out) is False
-
-
-def test_savedfiles():
-    ll = {"path": "./output/super_data888", "json": "create file super_data888"}
-    create_file(ll)
-    assert os.path.isfile(ll["path"]) is True
-
-
-def test_countfiles_multiprocessing():
-    count_files = 10
-    mydict = {"multiprocessing": 1, "data_schema": "./schema.json", "path_to_save_files": "./output",
-              "file_name": "super_data999", "file_count": count_files, "prefix": "count"}
-    cf_multiprocessing(mydict)
-    i = 0
-    for filename in os.listdir("./output"):
-        if filename.startswith("super_data999"):
-            i = i + 1
-    assert i == count_files
-
-
-def test_generate_json_file_name():
-    # test generate json name for files
-    i = 1
-    prefix = "count"
-    mydict = {"prefix": prefix, "path_to_save_files": "./output", "file_name": "test"}
-    assert generate_json_file_name(mydict, i) == "./output/test_1.json"
---
+++ tests.py
@@ -0,0 +1,72 @@
+from magicgenerator import read_json, cf_multiprocessing, clear_path, create_file, generate_json_file_name
+from unittest.mock import Mock
+import pytest
+import json
+import os
+
+DATA_SCHEM_FORTEST = "{\"date\": \"timestamp:\",\"name\": \"str:rand\",\"type\": " \
+                     "\"['client', 'partner', 'government']\",\"age\": \"int:rand(1, 15)\"}"
+
+
+@pytest.mark.parametrize("a, expected_result",
+                         [({'data_schema': "./schema.json"}, 100), ({'data_schema': DATA_SCHEM_FORTEST}, 100)])
+def test_datatype(a, expected_result):
+    assert type(read_json(a)['age']) is type(expected_result)
+
+
+@pytest.mark.parametrize("a, expected_result",
+                         [({'data_schema': "./schema.json"}, 100), ({'data_schema': DATA_SCHEM_FORTEST}, 100)])
+def test_dataschema(a, expected_result):
+    assert read_json(a)['age'] <= expected_result
+
+
+def test_temporaryfiles(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "schema.json"
+    m = Mock()
+    m.main(p)
+
+
+def test_clearpath():
+    output ="./output"
+    if not os.path.exists(output):
+        os.makedirs(output)
+    mydict = {"path_to_save_files": "./output", "file_name": "super_data777"}
+    out = mydict["path_to_save_files"] + "/" + mydict["file_name"]
+    with open(out, 'w', encoding="cp1251") as f:
+        json.dump(mydict, f, ensure_ascii=False)
+    clear_path(mydict)
+    assert os.path.isfile(out) is False
+
+
+def test_savedfiles():
+    output = "./output"
+    if not os.path.exists(output):
+        os.makedirs(output)
+    ll = {"path": "./output/super_data888", "json": "create file super_data888"}
+    create_file(ll)
+    assert os.path.isfile(ll["path"]) is True
+
+
+def test_countfiles_multiprocessing():
+    output = "./output"
+    if not os.path.exists(output):
+        os.makedirs(output)
+    count_files = 10
+    mydict = {"multiprocessing": 1, "data_schema": "./schema.json", "path_to_save_files": "./output",
+              "file_name": "super_data999", "file_count": count_files, "prefix": "count"}
+    cf_multiprocessing(mydict)
+    i = 0
+    for filename in os.listdir("./output"):
+        if filename.startswith("super_data999"):
+            i = i + 1
+    assert i == count_files
+
+
+def test_generate_json_file_name():
+    # test generate json name for files
+    i = 1
+    prefix = "count"
+    mydict = {"prefix": prefix, "path_to_save_files": "./output", "file_name": "test"}
+    assert generate_json_file_name(mydict, i) == "./output/test_1.json"
